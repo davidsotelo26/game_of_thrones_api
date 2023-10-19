7882214cdc455c2ad536a3895159c03eafa55fe2
davidsotelo26 <david.sotelo.rodriguez@bootcamp-upgrade.com>
2023-10-19 17:33:49 +0200

    Feat Folders

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Router, Routes } from 'react-router-dom';
+import CronologyPage from './pages/CronologyPage.jsx/CronologyPage';
+import HousesPage from './pages/HousesPage/HousesPage';
+import Menu from './components/Menu/Menu';
+import CharactersPage from './pages/CharactersPage/CharactersPage';
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <>
+        <Menu />
+
+        <Routes>
+          <Route path="/home" element={<HomePage/>}/>
+          <Route path="/characters" element={<CharactersPage/>}/>
+          <Route path="/houses" element={<HousesPage/>}/>
+          <Route path="/cronology" element={<CronologyPage/>}/>
+        </Routes>
+
+      </>
+    </Router>
   );
 }
 
---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+export default function Menu () {
+    return <body class="menu">
+
+        <Link to="characters">PERSONAJES</Link>
+        <Link to="houses">CASAS</Link>
+        <Link to="cronology">CRONOLOGIA</Link>
+        </body>
+    };
