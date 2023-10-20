5097bf6cc9fd3c270225d05dbe2685df069d8c0a
davidsotelo26 <david.sotelo.rodriguez@bootcamp-upgrade.com>
2023-10-20 12:07:36 +0200

    Feat CharacterInfoPage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import HousesPage from './pages/HousesPage/HousesPage';
 import CharactersPage from './pages/CharactersPage/CharactersPage';
 import HomePage from './pages/HomePage/HomePage';
 import "./style.css";
+import CharacterInfoPage from './pages/infopages/CharacterInfoPage/CharacterInfoPage';
+import HousesInfoPage from './pages/infopages/HousesInfoPage/HousesInfoPage';
 
 function App() {
   return (
@@ -12,10 +14,11 @@ function App() {
         <Routes>
           <Route path="" element={<HomePage/>}/>
           <Route path="characters" element={<CharactersPage/>}/>
+          <Route path="characters/:id" element={<CharacterInfoPage/>}/>
           <Route path="houses" element={<HousesPage/>}/>
+          <Route path="houses/:id" element={<HousesInfoPage/>}/>
           <Route path="cronology" element={<CronologyPage/>}/>
         </Routes>
-
       </div>
     </Router>
   );
