cda282d4e4019b996082d0931519f3ce1e17dc78
alvaro <alvarocliment1999@gmail.com>
2023-10-22 04:21:39 +0200

    houses 70% done

--- src/components/Gallery/HouseGallery.jsx
+++ src/components/Gallery/HouseGallery.jsx
@@ -1,1 +1,11 @@
-export default function HousesGallery () {}
+export default function HouseGallery ({data}) {
+    return (
+    <div className="houses-gallery">
+    {data.map((item, index) => (<div className="esc-name" key={index}>
+    <img className="houselogo" src={`http://localhost:3000/${item.image}`} alt="casas"/>
+    <p className="housename">{item.name}</p>
+    </div>
+    ))}
+</div>
+    )
+}
--- src/pages/HousesPage/HousesPage.jsx
+++ src/pages/HousesPage/HousesPage.jsx
@@ -1,2 +1,55 @@
-// import
-export default function HousesPage(){}
+import axios from "axios";
+import { useEffect, useState } from "react";
+import HouseGallery from "../../components/Gallery/HouseGallery";
+import "./HousePage.css"
+// import SearchBar from "../../components/SearchBar/SearchBar";
+import React from "react";
+import MenuInicio from "../../components/MenuInicio/MenuInicio";
+
+export default function HousesPage () {
+
+
+const [houses, setHouses] = useState([]);
+// const [housesFilter, setHousesFilter] = useState([]);
+
+// const [name, setName] = useState("");
+useEffect(()=>{
+    const getHouses = async () => {
+        const {data} = await axios.get(`http://localhost:3000/houses`)
+        setHouses(data);
+        // setHousesFilter(data);
+    }
+    getHouses();
+},[])
+// const handleSearch = (housesFilter) => {
+//     const filtered = houses.filter((house)=>house.name.toLowerCase().includes(housesFilter.toLowerCase()));
+//     setHousesFilter(filtered);
+// }
+
+// useEffect(() => {
+//     filterHouses();
+// },[])
+
+
+return (
+    <body className="body">
+        <nav className="navbar">
+        <div className="lupaybuscador">
+//         <img className="lupa" src="data:image/webp;base64,UklGRl4BAABXRUJQVlA4WAoAAAAQAAAAFAAAFAAAQUxQSBIBAAANkHPb1rHXonds2zYqp7Nt21ab1qls27Zt49q2fcMXiYgJAABOX1dd0VtrzaugG5AFvNlzNw34GvWMKk/ibto4cEclcCw35JAsiu84EgDOjppeTDcEfiTIpH/0B2X3fn1qOiEecaCZ45g//B6A9Y9BOmfNwWazAJR3QHvNWPcQAMsPeu+FhT4AeGvwmJYCXgPAlpPQMR07LBLafZIKaQg6P3hO6NEseDhHVYkMEM99y1faWy4IIuX4OkWCo6SxLr2i6Zd6YWZ4p9Tg/ZeAo5jM5CQLSeBB1mBJeZTXfwJupz5w1RE6HHoN+PlVB/mdEYB7fUcfQPzv69fk5X9GQvfMx73bw+9GOAu0GTS/GS5CTdwAVlA4ICYAAADQAgCdASoVABUAPt1qrlEopiQiqAgBEBuJaQAAPaOgAP7xbjUAAA==" alt="lupa"></img>
+//         <input className="buscador" type="text" placeholder="Buscar..."></input>
+//         </div>
+            {/* <SearchBar onSearch={handleSearch}/> */}
+            <p>home</p>
+            <p>banderitas</p>
+        </nav>
+        <div className="scrollable-container">
+            <HouseGallery data={houses}/>
+        </div>
+        <footer className="footer">
+            <div className="homebotones">
+                <MenuInicio/>
+            </div>
+        </footer>
+    </body>
+)
+}
